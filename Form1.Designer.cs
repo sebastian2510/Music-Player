e94af23d82cf1bf0ac1d0873b2d59619546b4c72
Sebastian-tec <67879028+WidovV@users.noreply.github.com>
2022-11-07 10:00:58 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MusicPlayer
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.SongList = new System.Windows.Forms.ListBox();
+            this.ImportSong = new System.Windows.Forms.Button();
+            this.MediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.RemoveSong = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.MediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SongList
+            // 
+            this.SongList.BackColor = System.Drawing.Color.DodgerBlue;
+            this.SongList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.SongList.FormattingEnabled = true;
+            this.SongList.Location = new System.Drawing.Point(614, 13);
+            this.SongList.Name = "SongList";
+            this.SongList.Size = new System.Drawing.Size(186, 390);
+            this.SongList.TabIndex = 5;
+            this.SongList.SelectedIndexChanged += new System.EventHandler(this.SongList_SelectedIndexChanged);
+            // 
+            // ImportSong
+            // 
+            this.ImportSong.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ImportSong.FlatAppearance.BorderSize = 0;
+            this.ImportSong.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ImportSong.Location = new System.Drawing.Point(717, 415);
+            this.ImportSong.Name = "ImportSong";
+            this.ImportSong.Size = new System.Drawing.Size(83, 33);
+            this.ImportSong.TabIndex = 6;
+            this.ImportSong.Text = "Import song(s)";
+            this.ImportSong.UseVisualStyleBackColor = false;
+            this.ImportSong.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MediaPlayer
+            // 
+            this.MediaPlayer.Enabled = true;
+            this.MediaPlayer.Location = new System.Drawing.Point(12, 12);
+            this.MediaPlayer.Name = "MediaPlayer";
+            this.MediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("MediaPlayer.OcxState")));
+            this.MediaPlayer.Size = new System.Drawing.Size(554, 391);
+            this.MediaPlayer.TabIndex = 7;
+            this.MediaPlayer.Enter += new System.EventHandler(this.MediaPlayer_Enter);
+            // 
+            // RemoveSong
+            // 
+            this.RemoveSong.BackColor = System.Drawing.Color.DodgerBlue;
+            this.RemoveSong.FlatAppearance.BorderSize = 0;
+            this.RemoveSong.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RemoveSong.Location = new System.Drawing.Point(614, 415);
+            this.RemoveSong.Name = "RemoveSong";
+            this.RemoveSong.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RemoveSong.Size = new System.Drawing.Size(97, 33);
+            this.RemoveSong.TabIndex = 8;
+            this.RemoveSong.Text = "Remove song(s)";
+            this.RemoveSong.UseVisualStyleBackColor = false;
+            this.RemoveSong.Click += new System.EventHandler(this.RemoveSong_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RemoveSong);
+            this.Controls.Add(this.MediaPlayer);
+            this.Controls.Add(this.ImportSong);
+            this.Controls.Add(this.SongList);
+            this.Name = "Form";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox SongList;
+        private System.Windows.Forms.Button ImportSong;
+        private AxWMPLib.AxWindowsMediaPlayer MediaPlayer;
+        private System.Windows.Forms.Button RemoveSong;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace MusicPlayer
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        string[] paths;
+        string[] files;
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void SongList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show($"PRE: {SongList.SelectedIndex}");
+            if (SongList.SelectedIndex > SongList.Items.Count || SongList.SelectedIndex < 0)
+            {
+                SongList.SelectedIndex = 0;
+            }
+            MediaPlayer.URL = paths[SongList.SelectedIndex];
+            MessageBox.Show($@"Song: {files[SongList.SelectedIndex]} | Path: {paths[SongList.SelectedIndex]}");
+        }
+
+        private void MediaPlayer_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            NameFilter filter = new NameFilter();
+
+            if (SongList.Items.Count > 0)
+            {
+                SongList.Items.Clear();
+            }
+
+
+            string cstring = "server = 192.168.16.178; uid = Sebastian; pwd = 123Abcd123; DATABASE = Music;";
+
+            SqlConnection conn = new SqlConnection(cstring);
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Songs", conn);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            string path = "";
+            string file = "";
+
+            while (dr.Read())
+            {
+                //MessageBox.Show($"Songs: {Convert.ToString(dr.GetValue(2))}");
+                path += Convert.ToString(@dr.GetValue(1)) + ";";
+                //MessageBox.Show($@"I: {index} | {paths[index]}");
+                file += Convert.ToString(dr.GetValue(2)) + ";";
+            }
+
+            paths = path.Split(';');
+            files = file.Split(';');
+            filter.Remover(files, out files);
+            for (int i = 0; i < files.Length; i++)
+            {
+
+                SongList.Items.Add(files[i]);
+            }
+
+            //MessageBox.Show($@"{paths[6]}");
+            dr.Close();
+            cmd.Cancel();
+            conn.Close();
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NameFilter filter = new NameFilter();
+            DBHandler db = new DBHandler();
+            OpenFileDialog ofd = new OpenFileDialog();
+            //ofd.Filter = ".mp3";
+            ofd.Multiselect = true;
+            
+            
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                files = ofd.SafeFileNames;
+                paths = ofd.FileNames;
+                
+                //MessageBox.Show($"Files: {Convert.ToString(files[0])}  | Paths: {Convert.ToString(paths[0])}");
+                
+                for (int i = 0; i < files.Length; i++)
+                {
+                    filter.Remover(files, out files);
+                    db.Insert(paths, files);
+                    SongList.Items.Add(files[i]);
+                }
+            }
+        }
+
+        private void RemoveSong_Click(object sender, EventArgs e)
+        {
+            string cstring = "server = 192.168.16.178; uid = Sebastian; pwd = 123Abcd123; DATABASE = Music;";
+
+            SqlCommand cmd = new SqlCommand();
+            SqlConnection conn = new SqlConnection(cstring);
+            conn.Open();
+
+            cmd = new SqlCommand($"DELETE FROM Songs WHERE song = '{files[SongList.SelectedIndex]}'", conn);
+            
+            int rows = cmd.ExecuteNonQuery();
+            SongList.Items.Remove(files[SongList.SelectedIndex]);
+            cmd.Cancel();
+
+            for (int i = 0; i < SongList.Items.Count; i++)
+            {
+                if (SongList.Items == null)
+                {
+                    SongList.Items.RemoveAt(i);
+                }
+            }
+
+            MessageBox.Show($"Delete song(s): {rows}");
+            conn.Close();
+        }
+    }
+    class DBHandler
+    {
+        /*
+        CREATE DATABASE Music;
+        CREATE TABLE Songs(
+        id int PRIMARY KEY IDENTITY(1,1),
+        path VARCHAR(100),
+        song VARCHAR(100),
+        Dato DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );
+        */
+
+        public void Insert(string[] paths, string[] files)
+        {
+            NameFilter filter = new NameFilter();
+            string cstring = "server = 192.168.16.178; uid = Sebastian; pwd = 123Abcd123; DATABASE = Music;";
+
+            SqlCommand cmd = new SqlCommand();
+            int rows = 0;
+            SqlConnection conn = new SqlConnection(cstring);
+            conn.Open();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                filter.Remover(files, out files);
+                cmd = new SqlCommand($"INSERT INTO Songs (path, song) VALUES ('{paths[i]}', '{files[i]}')", conn);
+                rows += cmd.ExecuteNonQuery();
+                cmd.Cancel();
+            }
+
+            MessageBox.Show($"Imported song(s): {rows}");
+            conn.Close();
+        }
+    }
+    class NameFilter
+    {
+        public void Remover(string[] files, out string[] output)
+        {
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (files[i].Contains('(') && files[i].Contains(')'))
+                {
+                    int start = files[i].LastIndexOf('(');
+                    int end = files[i].LastIndexOf(')');
+                    files[i] = files[i].Remove(start, end - start + 1);
+
+                    if (files[i].Contains('(') && files[i].Contains(')'))
+                    {
+                        start = files[i].LastIndexOf('(');
+                        end = files[i].LastIndexOf(')');
+                        files[i] = files[i].Remove(start, end - start + 1);
+
+                        if (files[i].Contains('(') && files[i].Contains(')'))
+                        {
+                            start = files[i].LastIndexOf('(');
+                            end = files[i].LastIndexOf(')');
+                            files[i] = files[i].Remove(start, end - start + 1);
+                        }
+                    }
+                }
+
+                if (files[i].Contains('[') && files[i].Contains(']'))
+                {
+                    int startbracket = files[i].LastIndexOf('[');
+                    int endbracket = files[i].LastIndexOf(']');
+                    files[i] = files[i].Remove(startbracket, endbracket - startbracket + 1);
+                }
+                /*
+                if (files[i].Contains(".mp3"))
+                {
+                    int start = files[i].IndexOf(".mp3");
+                    int end = files[i].LastIndexOf("mp3");
+                    files[i] = files[i].Remove(start, end - start + 3);
+                }
+                */
+            }
+            output = files;
+        }
+    }
+}
